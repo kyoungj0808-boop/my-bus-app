439f4fed6cad21483cd6770f644c32dfafd3313f
devjin_747 <kyoungj0808@gmail.com>
2026-06-14 03:29:26 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # [전역 설정]
-SYSTEM_MODE = 'SEOUL_PILOT' 
+SYSTEM_MODE = 'SEOUL' 
 st.set_page_config(page_title="서울 버스 정산 시스템", page_icon="🚌")
 
 # [관리자 전용] 시스템 통제실
@@ -9,12 +9,12 @@ admin_password = st.sidebar.text_input("관리자 마스터 코드", type="passw
 if st.sidebar.button("관리자 인증"):
     if admin_password == "3934":
         st.sidebar.success("시스템 통제 권한 활성화")
-        SYSTEM_MODE = st.sidebar.selectbox("지역 모드 선택", ['LOCKED', 'SEOUL_PILOT', 'BUSAN_LIVE'])
+        SYSTEM_MODE = st.sidebar.selectbox("지역 모드 선택", ['LOCKED', 'SEOUL', 'BUSAN'])
 
 # [지역 필터링]
 if SYSTEM_MODE == 'LOCKED':
     st.title("🚧 시스템 정비 중")
-elif SYSTEM_MODE == 'SEOUL_PILOT':
+elif SYSTEM_MODE == 'SEOUL':
     st.title("🚌 서울 기사님 전용 테스트 모드")
     password = st.text_input("기사님의 인증키를 입력하십시오", type="password")
     if st.button("인증 확인"):
@@ -25,7 +25,7 @@ elif SYSTEM_MODE == 'SEOUL_PILOT':
                 st.write("서울 노선 분석 완료.")
         else:
             st.warning("인증키가 틀렸습니다.")
-elif SYSTEM_MODE == 'BUSAN_LIVE':
+elif SYSTEM_MODE == 'BUSAN':
     st.title("🚌 부산 버스 정산 시스템")
     bus_number = st.text_input("버스 번호:")
     if st.button("정산 확인"):
